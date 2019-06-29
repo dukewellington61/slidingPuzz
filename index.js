48d83247185b98a00a3bd795bfc196343ae4ab51
dukewellington61 <dukewellington61@googlemail.com>
2019-06-30 01:24:00 +0200

    22

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { SlidingPuzzle } from "./SlidingPuzzle";
+
+ReactDOM.render(
+<SlidingPuzzle />,
+document.querySelector('#app')
+);
