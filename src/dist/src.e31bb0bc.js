df00378b54ac67f1ce6591275f40fbad71660bee
theIronDuke <dukewellington61@googlemail.com>
2019-08-30 10:01:52 +0200

    vertically centered

--- src/dist/src.e31bb0bc.js
+++ src/dist/src.e31bb0bc.js
@@ -31681,7 +31681,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39645" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46179" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
